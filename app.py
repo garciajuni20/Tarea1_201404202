1d9ea6f1af9c68c78de0472b193deecebd121f06
garciajuni20 <47872989+garciajuni20@users.noreply.github.com>
2024-02-07 18:19:31 -0600

    Agregado endpoint para añadir canciones

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Lista para almacenar las canciones
+canciones = []
+
+@app.route('/agregar', methods=['POST'])
+def agregar_cancion():
+    data = request.json
+    nueva_cancion = {
+        'nombre': data['nombre'],
+        'artista': data['artista'],
+        'album': data['album']
+    }
+    canciones.append(nueva_cancion)
+    return jsonify(nueva_cancion), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
